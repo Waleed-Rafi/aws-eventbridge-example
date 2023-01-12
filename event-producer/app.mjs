69665b9f873a56438553425da63481515e2f716d
Waleed Rafi <56467580+Waleed-Rafi@users.noreply.github.com>
2023-01-13 00:18:35 +0500

    implementation of producer and consumer functions

--- event-producer/app.mjs
+++ event-producer/app.mjs
@@ -3,24 +3,32 @@
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
+import AWS from "aws-sdk";
+
+const eventBridge = new AWS.EventBridge();
 
 export const lambdaHandler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  const data = { state: "update", key2: "value2" };
+  const params = {
+    Entries: [
+      {
+        Time: new Date().getTime(),
+        Source: "waleed.consumer",
+        Resources: [],
+        DetailType: "anyType",
+        Detail: JSON.stringify(data),
+        EventBusName: "default",
+        TraceHeader: "string",
+      },
+    ],
+  };
+  await eventBridge.putEvents(params).promise();
+  return { message: "Event Published" };
 };
